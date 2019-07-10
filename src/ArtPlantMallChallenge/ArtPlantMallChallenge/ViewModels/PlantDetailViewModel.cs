4ad750b7389410a0c82d1973428f7f6435a1044c
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2019-07-10 21:01:21 +0200

    Added SelectionChange logic to the plants collection

--- src/ArtPlantMallChallenge/ArtPlantMallChallenge/ViewModels/PlantDetailViewModel.cs
+++ src/ArtPlantMallChallenge/ArtPlantMallChallenge/ViewModels/PlantDetailViewModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ArtPlantMallChallenge.ViewModels
+﻿namespace ArtPlantMallChallenge.ViewModels
 {
     class PlantDetailViewModel
     {
